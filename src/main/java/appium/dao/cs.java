6345bca2d69ccf8398c94b4873fdb3ecc6f90dd9
w1q2q3q <1935417616@qq.com>
2019-01-02 11:59:00 +0800

    1

---
+++ src/main/java/appium/dao/cs.java
@@ -0,0 +1,19 @@
+package appium.dao;
+
+import java.util.*;
+
+public class cs extends demo {
+    @Override
+    void cs() {
+        List<String> c=new ArrayList<>();
+        List<String> cs=new LinkedList<>();
+        Map<String,String>cc= new HashMap<>();
+         Set<String>cq = new HashSet<>();
+     }
+
+    public static void main(String[] args) {
+        cs d =new cs();
+        d.cs();
+        d.css();
+    }
+}
---
+++ src/main/java/appium/dao/demo.java
@@ -0,0 +1,17 @@
+package appium.dao;
+
+import org.testng.*;
+import org.testng.xml.XmlSuite;
+
+import java.util.List;
+
+public abstract class demo {
+    abstract void cs();
+    void css(){
+
+        System.out.println("csssa");
+    }
+
+    public static void main(String[] args) {}
+
+}
