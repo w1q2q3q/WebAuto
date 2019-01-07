b173ade1f455c43bf868b00a8884e55ae3ffa38e
w1q2q3q <1935417616@qq.com>
2019-01-07 14:16:10 +0800

    1

--- src/main/java/appium/dao/cs.java
+++ src/main/java/appium/dao/cs.java
@@ -2,18 +2,21 @@ package appium.dao;
 
 import java.util.*;
 
-public class cs extends demo {
-    @Override
-    void cs() {
-        List<String> c=new ArrayList<>();
-        List<String> cs=new LinkedList<>();
-        Map<String,String>cc= new HashMap<>();
-         Set<String>cq = new HashSet<>();
-     }
+public class cs  {
+  public  static void cs() {
+      List<Double> cs = new ArrayList<>();
+      cs.add((double) 11);
+        cs.add((double) 14);
+        cs.add((double) 19);
+        cs.add((double) 9);
+        cs.add((double)5);
+        cs.add((double) 1);
+      Collections.sort(cs);
+      Collections.reverse(cs);
+      System.out.println(cs);
+}
 
     public static void main(String[] args) {
-        cs d =new cs();
-        d.cs();
-        d.css();
+cs();
     }
 }
